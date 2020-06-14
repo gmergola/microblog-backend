6a4a9e537bc4654a01d658d16aa6c30574e428b6
Genna Mergola <gennamergola@gmail.com>
2020-06-14 13:46:28 -0700

    deployment

--- backend/data.sql
+++ backend/data.sql
@@ -1,17 +1,17 @@
-DROP DATABASE IF EXISTS "microblog";
+-- DROP DATABASE IF EXISTS "microblog";
 
-CREATE DATABASE "microblog";
+-- CREATE DATABASE "microblog";
 
-\c "microblog"
+-- \c "microblog"
 
-CREATE TABLE posts (id SERIAL PRIMARY KEY, 
-                    title TEXT NOT NULL, 
+CREATE TABLE posts (id SERIAL PRIMARY KEY,
+                    title TEXT NOT NULL,
                     description TEXT NOT NULL,
-                    body TEXT, 
+                    body TEXT,
                     votes INT NOT NULL DEFAULT 0);
-                    
-CREATE TABLE comments (id SERIAL PRIMARY KEY, 
-                       text TEXT NOT NULL, 
+
+CREATE TABLE comments (id SERIAL PRIMARY KEY,
+                       text TEXT NOT NULL,
                        post_id INT NOT NULL REFERENCES posts ON DELETE CASCADE);
 
 INSERT INTO posts (title, description, body) VALUES
--- backend/db.js
+++ backend/db.js
@@ -6,5 +6,13 @@ const client = new Client(process.env.DATABASE_URL || "postgresql:///microblog")
 
 client.connect();
 
+let DB_URI;
+
+if (process.env.NODE_ENV === "test") {
+  DB_URI = "mircoblog-test";
+} else {
+  DB_URI  = process.env.DATABASE_URL || 'microblog';
+}
+
 
 module.exports = client;
